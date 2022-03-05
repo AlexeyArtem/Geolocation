348579b064f7762799f4b03ccea98d169a076bc8
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2022-03-05 12:14:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TestGeolocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geopoint point1 = new Geopoint("Москва", "Ленина", 37);
+            Geopoint point2 = new Geopoint("Брянск", "Ленина", 37);
+
+            double distance = point1.GetDistanceTo(point2) / 1000;
+
+            Console.WriteLine("Дистанция (км) = " + distance);
+
+            Console.Read();
+        }
+    }
+}
